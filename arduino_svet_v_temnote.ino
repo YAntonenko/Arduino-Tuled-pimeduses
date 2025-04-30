1cf1523a35013e349fddeaac31b26221152abc99
Yuna <yuna@hkhk.edu.ee>
2025-04-30 19:52:36 +0300

    Update arduino_svet_v_temnote.ino

--- arduino_svet_v_temnote.ino
+++ arduino_svet_v_temnote.ino
@@ -1,25 +1,28 @@
-int sensorPin = A0;      
-int ledPin = 2;          
-int valgustase;         
-int piir = 600;         
+int light = 0; 
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);    // Запуск монитора порта
+    // put your setup code here, to run once:
+    Serial.begin(9600); //configure serial to talk to computer
+    pinMode(7, OUTPUT); // configure digital pin 7 as an output
 }
 
 void loop() {
-  valgustase = analogRead(sensorPin); 
-  Serial.print("Уровень освещения: ");
-  Serial.println(valgustase);
-
-  if (valgustase < piir) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("ТЕМНО - СВЕТОДИОД ВКЛЮЧЕН");
-  } else {
-    digitalWrite(ledPin, LOW);
-    Serial.println("СВЕТЛО - СВЕТОДИОД ВЫКЛЮЧЕН");
-  }
-
-  delay(1000); 
+    // put your main code here, to run repeatedly:
+    light = analogRead(A0); // read and save value from PR
+    
+    Serial.println(light); // print current light value
+ 
+    if(light > 450) { // If it is bright...
+        Serial.println("It is quite light!");
+        digitalWrite(7,LOW); //turn  LED off
+    }
+    else if(light > 229 && light < 451) { // If it is average light...
+        Serial.println("It is average light!");
+       digitalWrite(7, HIGH); // turn LED on
+    }
+    else { // If it's dark...
+        Serial.println("It is pretty dark!");
+        digitalWrite(7,HIGH); // Turn  LED on
+    }
+    delay(1000); 
 }
